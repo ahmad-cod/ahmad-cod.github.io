493acb18812fe8d0fdeb43793a3205b0fee83956
aroyehunahmad <ahmadadewale800@gmail.com>
2022-03-18 09:32:22 +0100

    Replace buzzling prompt with console.log

--- js/web.js
+++ js/web.js
@@ -1,4 +1,1 @@
-let visitor=prompt('what is your name?');
-alert('Hi' + " " + visitor);
-alert('Welcome Home ' + visitor+ '!');
-alert('I am Ahmad Aroyehun, Nice meeting you!')
+console.log('Thanks for visiting!!')
